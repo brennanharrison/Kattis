51a282cc4e03464626bf60b96de6731773b743fb
brennanharrison <54481933+brennanharrison@users.noreply.github.com>
2020-02-28 16:43:27 -0500

    Add files via upload

--- dasblinkenlights.py
+++ dasblinkenlights.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[37]:
+# In[41]:
 
 
 p,q,s= [int(x) for x in input().split()] # Get the data
@@ -14,13 +14,14 @@ y = list(range(0,s+1,q)) # Build a range of multiples for q until the last eleme
 k = k[1:] # Remove first element from first range.
 y = y[1:] # Remove first element from second range.
 
-u = [i for i in k + y if i in k and i in y] # Concatenate first range to second range and only return elements that exist in both lists.
+u = [i for i in k + y if i in k and i in y and i <= s] # Concatenate first range to second range and only 
+                                                        # return elements that exist in both lists and are <= s.
+
 u = set(u) # Return only unique elements.
 
-j = [i for i in u if i <= s] # Return elements <= s.
 
 # If multiples exist in both initial ranges that are <= s, print.
-if len(j) > 0:
+if len(u) > 0:
     print('yes')
 else:
     print('no')
